a0fb5142397b9136db86c2baf288f93a755d91c1
Jesse <jchisu31@gmail.com>
2015-04-09 09:16:47 -0700

    Awesomenauts- Update Day 20

--- js/entities/GameManager.js
+++ js/entities/GameManager.js
@@ -5,22 +5,19 @@ game.GameTimerManager = Object.extend({
         this.paused = false;
         this.alwaysUpdate = true;
     },
-    
     update: function() {
-        this.now = new Date().getTime();        
-        this.goldTimerCheck();        
-        this.creepTimerCheck();                
+        this.now = new Date().getTime();
+        this.goldTimerCheck();
+        this.creepTimerCheck();
         return true;
     },
-    
     goldTimerCheck: function() {
-        if (Math.round(this.now / 1000) %20 === 0 && (this.now - this.lastCreep >= 1000)) {
-            game.data.gold += (game.data.exp1+1);
+        if (Math.round(this.now / 1000) % 20 === 0 && (this.now - this.lastCreep >= 1000)) {
+            game.data.gold += (game.data.exp1 + 1);
         }
     },
-    
     creepTimerCheck: function() {
-        if (Math.round(this.now / 1000) %10 === 0 && (this.now - this.lastCreep >= 1000)) {
+        if (Math.round(this.now / 1000) % 10 === 0 && (this.now - this.lastCreep >= 1000)) {
             this.lastCreep = this.now;
             var creepe = me.pool.pull("EnemyCreep", 1000, 0, {});
             me.game.world.addChild(creepe, 5);
@@ -32,9 +29,8 @@ game.HeroDeathManager = Object.extend({
     init: function(x, y, settings) {
         this.alwayeUpdate = true;
     },
-    
-    update: function() {                
-        if(game.data.player.dead) {
+    update: function() {
+        if (game.data.player.dead) {
             me.game.world.removeChild(game.data.player);
             me.state.current().resetPlayer(0, 620);
         }
@@ -46,24 +42,22 @@ game.ExperienceManager = Object.extend({
         this.alwaysUpdtae = true;
         this.gameover = false;
     },
-    
     update: function() {
-        if(game.data.win === true && !this.gameover) {
+        if (game.data.win === true && !this.gameover) {
             this.gameOver(true);
-        }else if(game.data.win === false && !this.gameover) {
+        } else if (game.data.win === false && !this.gameover) {
             this.gameOver(false);
         }
         return false;
     },
-    
     gameOver: function(win) {
-        if(win) {
+        if (win) {
             game.data.exp += 10;
-        }else{
+        } else {
             game.data.exp += 1;
-        }        
-            this.gameOver = true;
-            me.save.exp = game.data.exp;
+        }
+        this.gameOver = true;
+        me.save.exp = game.data.exp;
     }
 });
 
@@ -75,12 +69,74 @@ game.SpendGold = Object.extend({
         this.alwaysUpdate = true;
         this.updateWhenPaused = true;
     },
-    
     update: function() {
         this.now = new Date().getTime();
-        if(me.input.isKeyPressed("buy")) {
-            
+        if (me.input.isKeyPressed("buy") && this.now - this.lastBuy >= 1000) {
+            this.lastBuy = this.now;
+            if (!this.buying) {
+                this.startBuying();
+            } else {
+                this.stopBuying();
+            }
         }
         return true;
+    },
+    startBuying: function() {
+        this.buying = true;
+        me.state.pause(me.state.PLAY);
+        game.data.pausePos = me.game.viewport.localToWorld(0, 0);
+        game.data.buyscreen = new me.Sprite(game.data.pausePos.x, game.data.pausePos.y, me.loader.getImage("gold-screen"));
+        game.data.buyscreen.updateWhenPaused = true;
+        game.data.buyscreen.setOpacity(0.8);
+        game.data.player.body.setVelocity(0, 0);
+        me.game.world.addChild(game.data.buyscreen, 34);
+        this.setKeys();
+        this.setBuyText();
+    },
+    setKeys: function() {
+        me.input.bindKey(me.input.KEY.F1, "F1", true);
+        me.input.bindKey(me.input.KEY.F2, "F2", true);
+        me.input.bindKey(me.input.KEY.F3, "F3", true);
+        me.input.bindKey(me.input.KEY.F4, "F4", true);
+        me.input.bindKey(me.input.KEY.F5, "F5", true);
+        me.input.bindKey(me.input.KEY.F6, "F6", true);
+    },
+    
+    setBuyText: function() {
+        game.data.buytext = new (me.Renderable.extend({
+            init: function() {
+                this._super(me.Renderable, 'init', [10, 10, 300, 50]);
+                this.font = new me.Font('Times New Roman', 26, 'white');
+                this.updateWhenPaused = true;
+                this.alwaysUpdate = true;
+            },
+            draw: function(renderer) {
+                this.font.draw(renderer.getContext(), "Press F1-F6 to SELECT, and B to EXIT", this.pos.x, this.pos.y + 60);
+                this.font.draw(renderer.getContext(), "Press F1-F6 to SELECT, and B to EXIT", this.pos.x + 40, this.pos.y + 20);
+                this.font.draw(renderer.getContext(), "Press F1-F6 to SELECT, and B to EXIT", this.pos.x + 80, this.pos.y + 20);
+                this.font.draw(renderer.getContext(), "Press F1-F6 to SELECT, and B to EXIT", this.pos.x + 120, this.pos.y + 20);
+                this.font.draw(renderer.getContext(), "Press F1-F6 to SELECT, and B to EXIT", this.pos.x + 160, this.pos.y + 20);
+                this.font.draw(renderer.getContext(), "Press F1-F6 to SELECT, and B to EXIT", this.pos.x + 200, this.pos.y + 20);
+
+            }
+        }));
+        me.game.world.addChild(game.data.buytext, 35);
+    },
+    stopBuying: function() {
+        this.buying = false;
+        me.state.resume(me.state.PLAY);
+        game.data.player.body.setVelocity(game.data.playerMoveSpeed, 20);
+        me.game.world.removeChild(game.data.buyscreen);
+        this.unsetKeys();
+        me.game.world.removeChild(game.data.buytext);
+    },
+    
+    unsetKeys: function() {
+        me.input.unbindKey(me.input.KEY.F1, "F1", true);
+        me.input.unbindKey(me.input.KEY.F2, "F2", true);
+        me.input.unbindKey(me.input.KEY.F3, "F3", true);
+        me.input.unbindKey(me.input.KEY.F4, "F4", true);
+        me.input.unbindKey(me.input.KEY.F5, "F5", true);
+        me.input.unbindKey(me.input.KEY.F6, "F6", true);
     }
 });
